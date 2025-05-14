05ab3d9f83e624052130b499559eb96e35efd4a3
Serrano Horacio <serranohoraciohector@gmail.com>
2025-05-14 19:19:39 -0300

    Añade middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var methodOverride = require('method-override')
 const app = express()
 const config = require('dotenv').config({ path: __dirname + '/.env' }).parsed;
 const port = config.PUERTO;
@@ -6,6 +7,9 @@ const port = config.PUERTO;
 
 //parseo request body
 app.use(express.json());
+app.use(express.urlencoded());
+app.use(methodOverride('X-HTTP-Method-Override'));
+
 //utiliza CORS
 var cors = require('cors');
 app.use(cors());
